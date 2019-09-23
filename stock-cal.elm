6f190985ded849034bb53310caa3c0c3fee1a104
AshineFoster <ashinefoster@gmail.com>
2019-09-23 16:24:04 -0500

    working calculator full price

--- stock-cal.elm
+++ stock-cal.elm
@@ -64,6 +64,8 @@ view model =
         , viewInput "text" "price per stock" model.pricePerStock PricePerStock
         , div [] [ text "Max # of stocks:" ]
         , div [] [ text <| calculateMaxShares model.avaliableCash model.pricePerStock ]
+
+        -- , div [ style "font-family" "sans-serif" ] [ text <| String.append "Total for 10,000 shares at $1.75: $" (Decimal.toString <| fullCal 10000 (Maybe.withDefault (Decimal.minusOne) (1.75 |> Decimal.fromFloat))) ]
         ]
 
 
@@ -72,14 +74,14 @@ viewInput t p v toMsg =
     input [ type_ t, placeholder p, value v, onInput toMsg ] []
 
 
-toCents : String -> Decimal
-toCents val =
-    Maybe.withDefault (Decimal.fromInt 0) (Decimal.fromString val)
+strToDecimal : String -> Decimal
+strToDecimal val =
+    Maybe.withDefault Decimal.zero (Decimal.fromString val)
 
 
-commissionPercent : Decimal
-commissionPercent =
-    Maybe.withDefault (Decimal.fromInt -1) (0.02 |> Decimal.fromFloat)
+commission : Decimal
+commission =
+    Maybe.withDefault Decimal.minusOne (0.0002 |> Decimal.fromFloat)
 
 
 tradeFee : Decimal
@@ -87,33 +89,77 @@ tradeFee =
     1000 |> Decimal.fromInt
 
 
-cessFeePercent : Decimal
-cessFeePercent =
-    Maybe.withDefault (Decimal.fromInt -1) (0.3 |> Decimal.fromFloat)
+cessFee : Decimal
+cessFee =
+    Maybe.withDefault Decimal.minusOne (0.0033 |> Decimal.fromFloat)
 
 
-gctPercent : Decimal
-gctPercent =
-    Maybe.withDefault (Decimal.fromInt -1) (16.5 |> Decimal.fromFloat)
+gct : Decimal
+gct =
+    Maybe.withDefault Decimal.minusOne (0.165 |> Decimal.fromFloat)
+
+
+minShares : Decimal
+minShares =
+    Decimal.fromInt 100
+
+
+minCommission : Decimal
+minCommission =
+    Decimal.fromInt 500
 
 
 calculateMaxShares : String -> String -> String
 calculateMaxShares avaliableCash pricePerStock =
     let
         cash =
-            toCents avaliableCash
+            Decimal.truncate -2 (strToDecimal avaliableCash)
 
         price =
-            toCents pricePerStock
+            Decimal.truncate -2 (strToDecimal pricePerStock)
 
-        maxAmount =
-            Maybe.withDefault (Decimal.fromInt 0) (Decimal.fastdiv cash price)
+        maxShares =
+            Maybe.withDefault Decimal.zero (Decimal.fastdiv cash price)
 
         minAmountPrice =
-            price |> Decimal.mul (toCents "100")
+            fullCal minShares price
     in
     if Decimal.gt minAmountPrice cash then
         "Not enought funds to buy shares."
 
     else
-        Decimal.truncate 0 maxAmount |> Decimal.toString
+        Decimal.truncate 0 (findMaxShares maxShares price cash) |> Decimal.toString
+
+
+findMaxShares : Decimal -> Decimal -> Decimal -> Decimal
+findMaxShares maxShares price cash =
+    price
+
+
+fullCal : Decimal -> Decimal -> Decimal
+fullCal numOfShares pricePerStock =
+    let
+        grossPrice =
+            Decimal.mul pricePerStock numOfShares
+
+        finalCommission =
+            if Decimal.lte (Decimal.mul grossPrice commission) minCommission then
+                minCommission
+
+            else
+                pricePerStock
+
+        cess =
+            Decimal.mul grossPrice cessFee
+
+        trade =
+            Decimal.zero
+
+        gctTax =
+            Decimal.add finalCommission cess |> Decimal.add trade |> Decimal.mul gct
+    in
+    Decimal.add grossPrice cess
+        |> Decimal.add trade
+        |> Decimal.add gctTax
+        |> Decimal.add finalCommission
+        |> Decimal.round -2
